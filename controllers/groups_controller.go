c61fccfded8aed64dcc973bedd0b3f8735cd87f2
Philip Giuliani <info@philipg.net>
2016-01-18 23:00:40 +0100

    Add upper.io with example

--- controllers/groups_controller.go
+++ controllers/groups_controller.go
@@ -1,23 +1,33 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/philipgiuliani/rustplanner/models"
 	"github.com/unrolled/render"
+	"upper.io/db"
 )
 
 type GroupsController struct {
 	AppController
 	*render.Render
+	DB db.Database
 }
 
 func (c *GroupsController) Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	group := models.Group{
-		Name: "cortex",
+	var groups models.Groups
+
+	groupsCollection, err := c.DB.Collection("groups")
+	if err != nil {
+		log.Fatalf("DB.Collection(): %q\n", err)
 	}
-	c.JSON(w, http.StatusOK, group)
+
+	res := groupsCollection.Find()
+	res.All(&groups)
+
+	c.JSON(w, http.StatusOK, groups)
 }
 
 func (c *GroupsController) Create(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/codegangsta/negroni"
 	"github.com/julienschmidt/httprouter"
 	"github.com/philipgiuliani/rustplanner/controllers"
 	"github.com/unrolled/render"
+	"upper.io/db"
+	"upper.io/db/sqlite"
 )
 
 func main() {
+	// render, middleware
 	r := render.New(render.Options{})
 
 	n := negroni.New(
@@ -15,9 +20,21 @@ func main() {
 		negroni.NewRecovery(),
 	)
 
+	// database
+	dbSettings := sqlite.ConnectionURL{
+		Database: "test.db",
+	}
+
+	sess, err := db.Open(sqlite.Adapter, dbSettings)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sess.Close()
+
+	// router
 	router := httprouter.New()
 
-	groupsController := &controllers.GroupsController{Render: r}
+	groupsController := &controllers.GroupsController{Render: r, DB: sess}
 	router.GET("/api/groups.json", groupsController.Index)
 	router.POST("/api/groups.json", groupsController.Create)
 
